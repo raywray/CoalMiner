59c9610a35fc071bce9401433c6d2a54de95e10b
Your Name <resplin5072@sdsu.edu>
2024-08-20 17:32:21 -0700

    deletes .sh file

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf output/
-
-cp example_input_files/hops_* .
-
-python3 coalminer.py /home/raya/Documents/Projects/CoalMiner/example_input_files/user_input_hops_k4.yml
-
-rm -rf hops_*
